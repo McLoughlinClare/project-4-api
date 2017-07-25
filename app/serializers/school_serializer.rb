9b64a50dbe79b13cdce0887efdd3544556f088cd
McLoughlinClare <mcloughlinclare@yahoo.com>
2017-07-25 11:56:40 +0100

    school scores and average schools added

--- app/serializers/school_serializer.rb
+++ app/serializers/school_serializer.rb
@@ -1,5 +1,17 @@
 class SchoolSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :total_correct_answers, :average_correct
   has_many :teachers
   has_many :students
+
+  def total_correct_answers
+    object.students.reduce(0) do |acc, student|
+      correct_answers = student.solutions.select(&:correct).length
+      acc + correct_answers
+    end
+  end
+
+def average_correct
+  total_correct_answers / object.students.length.to_f
+end
+
 end
--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,6 +1,10 @@
 class StudentSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :password_digest
+  attributes :id, :first_name, :last_name, :email, :password_digest, :correct_answers
   has_one :school
   has_many :solutions, through: :challenge
   has_many :teachers, through: :school
+
+  def correct_answers
+    object.solutions.select(&:correct).length
+  end
 end
